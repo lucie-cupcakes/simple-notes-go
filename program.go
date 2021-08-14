9ebba6eca8c81cf36aefe071b1cb854ceccedcd9
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-08-14 16:36:41 +0000

    fast_commit at 20210814_163641

--- program.go
+++ program.go
@@ -9,9 +9,10 @@ import (
 
 // Program represents the current program and its state
 type Program struct {
-	dbHandle     *PepinoDB
-	noteList     *NoteList
-	commandInput string
+	dbHandle  *PepinoDB
+	noteList  *NoteList
+	cmdInput  string
+	cmdReader *bufio.Reader
 }
 
 // Initialize sets up the initial values for the Program instance
@@ -23,11 +24,40 @@ func (p *Program) Initialize() error {
 	nl.Initialize()
 	nl.Load(p.dbHandle)
 	p.noteList = &nl
+	p.cmdReader = bufio.NewReader(os.Stdin)
 	return nil
 }
 
-func (p *Program) newCommand() {
+func (p *Program) readUntilFinish() string {
+	fmt.Println("TIP: type @finish@ when you end the Note.")
+	contentsSb := strings.Builder{}
+	for {
+		line, _ := p.cmdReader.ReadString('\n')
+		if strings.HasPrefix(line, "@finish@") {
+			break
+		}
+		contentsSb.WriteString(line)
+	}
+	return contentsSb.String()
+}
 
+func (p *Program) newCommand() {
+	fmt.Print("title: ")
+	title, _ := p.cmdReader.ReadString('\n')
+	contents := p.readUntilFinish()
+	note := Note{}
+	note.Create(title, contents)
+	err := note.Save(p.dbHandle)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	p.noteList.Value[note.ID.String()] = note.Title
+	err = p.noteList.Save(p.dbHandle)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 }
 
 func (p *Program) deleteCommand() {
@@ -43,7 +73,13 @@ func (p *Program) printCommand() {
 }
 
 func (p *Program) listCommand() {
-
+	if len(p.noteList.Value) > 0 {
+		for noteID, noteTitle := range p.noteList.Value {
+			fmt.Println(noteID + "\t" + noteTitle)
+		}
+	} else {
+		fmt.Println("There are not saved notes.")
+	}
 }
 
 func (p *Program) helpCommand() {
@@ -56,36 +92,27 @@ func (p *Program) helpCommand() {
 		"exit-\tLeave the program.")
 }
 
-func (p *Program) readUntilFinish(reader *bufio.Reader) {
-	contentsSb := strings.Builder{}
-	for {
-		line, _ := reader.ReadString('\n')
-		if strings.HasPrefix(line, "@finish@") {
-			break
-		}
-		contentsSb.WriteString(line)
-	}
-}
-
 // Run is the main function for the Program object
 func (p *Program) Run() {
-	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Welcome to the Notes program!\n" +
 		"TIP: type help for the command list.")
 	for {
-		p.commandInput, _ = reader.ReadString('\n')
-		if strings.HasPrefix(p.commandInput, "new") {
+		fmt.Print("Notes>")
+		p.cmdInput, _ = p.cmdReader.ReadString('\n')
+		if strings.HasPrefix(p.cmdInput, "new") {
 			p.newCommand()
-		} else if strings.HasPrefix(p.commandInput, "del") {
+		} else if strings.HasPrefix(p.cmdInput, "del") {
 			p.deleteCommand()
-		} else if strings.HasPrefix(p.commandInput, "mod") {
+		} else if strings.HasPrefix(p.cmdInput, "mod") {
 			p.modifyCommand()
-		} else if strings.HasPrefix(p.commandInput, "list") {
+		} else if strings.HasPrefix(p.cmdInput, "list") {
 			p.listCommand()
-		} else if strings.HasPrefix(p.commandInput, "print") {
+		} else if strings.HasPrefix(p.cmdInput, "print") {
 			p.printCommand()
-		} else if strings.HasPrefix(p.commandInput, "help") {
+		} else if strings.HasPrefix(p.cmdInput, "help") {
 			p.helpCommand()
+		} else if strings.HasPrefix(p.cmdInput, "exit") {
+			break
 		}
 	}
 }
